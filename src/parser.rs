2dee234c773aadb74443a3750f12ee171b6d153d
Graham Misail <gmisail64@gmail.com>
2023-01-07 15:45:33 -0500

    Parser: implement peek, next, & match

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 use super::ast;
+use super::token;
 use super::scanner;
 use super::token::{Token, TokenKind};
 
@@ -14,11 +15,35 @@ impl Parser {
             None => true,
         }
     }
+
+    fn peek_token(&self) -> Option<&token::Token> {
+        self.tokens.get(self.current) 
+    }
+
+    fn next_token(&mut self) -> Option<&token::Token> {
+        self.current += 1;
+        self.peek_token()
+    }
+
+    fn match_token(&self, kind: token::TokenKind) -> bool {
+        let peeked = self.peek_token();
+        
+        match peeked {
+            Some(t) => t.kind == kind,
+            None => false
+        }
+    }
 }
 
 pub fn parse(input: String) -> Result<ast::Expression, &'static str> {
     let tokens = scanner::scan(input)?;
-    let context = Parser { current: 0, tokens };
+    let mut context = Parser { current: 0, tokens };
+
+    while !context.is_at_end() {
+        println!("token: {}", context.peek_token().unwrap().kind);
+        
+        context.next_token();
+    }
 
     Ok(
         ast::Expression::Binary(
--- src/token.rs
+++ src/token.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 pub enum TokenKind {
     // Symbols (single-width)
     LeftParen,
