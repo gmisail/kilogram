d00f2978b06239ccce9c39e3c38009e4f2e93ba4
Graham Misail <gmisail64@gmail.com>
2023-03-22 23:44:14 -0400

    Postprocessor: implement FreshGenerator as an iterator

--- src/fresh/generator.rs
+++ src/fresh/generator.rs
@@ -11,12 +11,16 @@ impl FreshGenerator {
     pub fn new(subject: &'static str) -> Self {
         FreshGenerator { subject, index: 0 }
     }
+}
+
+impl Iterator for FreshGenerator {
+    type Item = String;
 
     /// Generates a new fresh variable name.
-    pub fn next(&mut self) -> String {
+    fn next(&mut self) -> Option<Self::Item> {
         self.index += 1;
 
-        format!("_kg_{}_{}", self.subject, self.index - 1)
+        Some(format!("_kg_{}_{}", self.subject, self.index - 1))
     }
 }
 
@@ -28,14 +32,14 @@ mod tests {
     fn generates_fresh_variable() {
         let mut test_gen = FreshGenerator::new("test");
 
-        assert_eq!("_kg_test_0", test_gen.next());
-        assert_eq!("_kg_test_1", test_gen.next());
-        assert_eq!("_kg_test_2", test_gen.next());
+        assert_eq!("_kg_test_0", test_gen.next().unwrap());
+        assert_eq!("_kg_test_1", test_gen.next().unwrap());
+        assert_eq!("_kg_test_2", test_gen.next().unwrap());
 
         let mut var_gen = FreshGenerator::new("var");
 
-        assert_eq!("_kg_var_0", var_gen.next());
-        assert_eq!("_kg_var_1", var_gen.next());
-        assert_eq!("_kg_var_2", var_gen.next());
+        assert_eq!("_kg_var_0", var_gen.next().unwrap());
+        assert_eq!("_kg_var_1", var_gen.next().unwrap());
+        assert_eq!("_kg_var_2", var_gen.next().unwrap());
     }
 }
